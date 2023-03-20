e93c89e5c111bc7a06de2e1205c204509c6e1c8e
Valentina <massavalen.a@gmail.com>
2023-03-20 12:35:54 -0300

    buttons

---
+++ src/Components/Buttons.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './Buttons.css'
+
+function Buttons(){
+
+ 
+
+
+    return(
+        <div class="container">
+  <div class="centered-div">
+        <div class="button-container">
+        <div class="button-group">
+          <button>Button 1</button>
+          <button>Button 2</button>
+        </div>
+        <div class="button-group">
+          <button>Button 3</button>
+          <button>Button 4</button>
+        </div>
+      </div>
+      </div>
+      </div>      
+        
+    )
+
+}
+
+export default Buttons
